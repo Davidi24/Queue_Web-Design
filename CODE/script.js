0e160892bf395644c6dc4853656ff832fd8b502a
David Keci <dkeci21@epoka.edu.al>
2023-05-28 01:51:40 +0200

    David changes

--- CODE/script.js
+++ CODE/script.js
@@ -25,10 +25,10 @@ class Node {
 	setPrev(p) {
 	  this.prev = p;
 	}
-  }
+}
   
-  // DoublyLinkedList class
-  class DoubleLinkedList {
+// DoublyLinkedList class
+class DoubleLinkedList {
 	constructor() {
 	  this.header = new Node(null, null, null);
 	  this.trailer = new Node(null, this.header, null);
@@ -46,7 +46,7 @@ class Node {
   
 	getFirst() {
 	  if (!this.isEmpty()) {
-		return this.header.next.getElement();
+		return this.header.next;
 	  } else {
 		return null;
 	  }
@@ -54,7 +54,7 @@ class Node {
   
 	getLast() {
 	  if (!this.isEmpty()) {
-		return this.trailer.prev.getElement();
+		return this.trailer.prev;
 	  } else {
 		return null;
 	  }
@@ -86,7 +86,7 @@ class Node {
   
 	removeFirst() {
 	  if (this.isEmpty()) {
-		return;
+		return null;
 	  }
 	  const first = this.header.next;
 	  this.header.setNext(first.getNext());
@@ -94,75 +94,109 @@ class Node {
 	  first.setNext(null);
 	  first.setPrev(null);
 	  this.size--;
+	  return first.getElement();
 	}
   
-	show() {
-	  const arr = [];
-	  if (!this.isEmpty()) {
-		let temp = this.header.next;
-		while (temp !== this.trailer) {
-		  arr.push(temp.getElement());
-		  temp = temp.getNext();
-		}
+	getElements() {
+	  const elements = [];
+	  let current = this.header.next;
+	  while (current !== this.trailer) {
+		elements.push(current.getElement());
+		current = current.getNext();
 	  }
-	  console.log(arr);
+	  return elements;
 	}
-  }
+}
   
-  // Queue class
-  class Queue {
+// Queue class
+class Queue {
 	constructor() {
 	  this.list = new DoubleLinkedList();
-	  this.size = 0;
-	}
-  
-	push(element) {
-	  this.list.addLast(element);
-	  this.size++;
-	}
-  
-	pop() {
-	  if (this.isEmpty()) {
-		console.log("Queue Empty");
-		return null;
-	  }
-	  const temp = this.list.getFirst();
-	  this.list.removeFirst();
-	  this.size--;
-	  return temp;
-	}
-  
-	show() {
-	  this.list.show();
+	  this.front = null;
+	  this.rear = null;
+	  this.updateVisualization();
 	}
   
 	isEmpty() {
-	  return this.size === 0;
+	  return this.list.isEmpty();
 	}
   
-	Size() {
-	  return this.size;
+	size() {
+	  return this.list.getSize();
 	}
-  }
   
-  const button = document.querySelector('.Push');
+	enqueue(element) {
+	  this.list.addLast(element);
+	  this.front = this.list.getFirst();
+	  this.rear = this.list.getLast();
+	  this.updateVisualization();
+	}
   
-  button.addEventListener('click', (event) => {
-	event.preventDefault();
-	var q1 = new Queue();
-	q1.push(1);
-	q1.push(2);
-	q1.push(3);
-	q1.show();
-	q1.pop();
-	q1.show();
-	q1.pop();
-	q1.pop();
-	q1.pop();
-	console.log(q1.isEmpty());
-	console.log(q1.Size());
-	q1.push(5);
-	console.log(q1.Size());
+	dequeue() {
+	  if (this.isEmpty()) {
+		console.log('Queue is empty');
+		return null;
+	  }
+	  const dequeuedElement = this.list.removeFirst();
+	  this.front = this.list.getFirst();
+	  this.rear = this.list.getLast();
+	  this.updateVisualization();
+	  return dequeuedElement;
+	}
+  
+	getFront() {
+	  if (this.isEmpty()) {
+		console.log('Queue is empty');
+		return null;
+	  }
+	  return this.front.getElement();
+	}
+  
+	getRear() {
+	  if (this.isEmpty()) {
+		console.log('Queue is empty');
+		return null;
+	  }
+	  return this.rear.getElement();
+	}
+  
+	updateVisualization() {
+		const queueContainer = document.querySelector('.visualisation');
+		queueContainer.innerHTML = '';
+	  
+		const elements = this.list.getElements();
+		
+		const linesContainer = document.createElement('div');
+		linesContainer.className = 'lines-container';
+		queueContainer.appendChild(linesContainer);
+	  
+		for (let i = 0; i < elements.length; i++) {
+		  const element = elements[i];
+	  
+		  const box = document.createElement('div');
+		  box.className = 'box';
+		  box.textContent = element;
+		  queueContainer.appendChild(box);
+	  
+		  if (i !== elements.length - 1) {
+			const line = document.createElement('div');
+			line.className = 'line';
+			linesContainer.appendChild(line);
+		  }
+		}
+	  }
+}
+  
+// Event listener for the form submission
+document.querySelector('form').addEventListener('submit', function (e) {
+	e.preventDefault();
+	const input = document.querySelector('.ele1');
+	const element = parseInt(input.value);
 	
-	
+	if (!isNaN(element)) {
+	  queue.enqueue(element);
+	  input.value = '';
+	}
   });
+// Create an instance of the Queue class
+const queue = new Queue();
